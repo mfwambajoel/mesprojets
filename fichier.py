9a8690db7cc67bbaba8520a5796d048294a4253e
mfwambajoel <jlmfwamba@gmail.com>
2022-12-30 16:17:31 +0100

    init

---
+++ fichier.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 18:05:53 2022
+
+@author: joel mfwamba
+"""
+
+def impr(nombre):
+    """Fonction qui permet d'imprimer les + sous forme matricielle"""
+    
+    
+    
+    while 0<nombre:
+        print(i)
+        
+        nombre-=1
+try:        
+
+    nombre=int(input("Entrer un nombre naturel: "))
+
+    i="+"*nombre
+       
+    impr(nombre)
+    
+except:
+    print("Entrée invalide !")
